35f6ed7b3e4b500ef1cb30d55870fe6a30a8bc17
TimoFeindt <93056175+TimoFeindt@users.noreply.github.com>
2024-11-15 19:37:41 +0100

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+	return (
+		<>
+			<div className='intro'>
+				<h1>Hi I am Timo</h1>
+				<span>Timo Feindt</span>
+				<span>and I want to make the web available for everyone!</span>
+			</div>
+		</>
+	);
+}
